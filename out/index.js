f985bacf09ae02e1f29d6e017b61926fdd8f15f6
Tatiane Oliveira <164065945+TatianeOliveira8@users.noreply.github.com>
2025-04-09 20:08:12 -0300

    operacao básica, modulo

---
+++ out/index.js
@@ -0,0 +1,29 @@
+class Somador {
+    constructor() {
+        this.calcular = (numero1, numero2) => {
+            return numero1 + numero2;
+        };
+    }
+}
+class Subtrador {
+    constructor() {
+        this.calcular = (numero1, numero2) => {
+            return numero1 - numero2;
+        };
+    }
+}
+class Divisor {
+    constructor() {
+        this.calcular = (numero1, numero2) => {
+            return numero1 / numero2;
+        };
+    }
+}
+class Multiplicador {
+    constructor() {
+        this.calcular = (numero1, numero2) => {
+            return numero1 * numero2;
+        };
+    }
+}
+export { Somador, Subtrador, Divisor, Multiplicador };
